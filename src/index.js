8b50f064582e42e677c67bdefd6ae3da6f077884
foraminifer <87345841+foraminifer@users.noreply.github.com>
2021-12-12 17:47:23 -0700

    Removed inaccurate icons

--- src/index.js
+++ src/index.js
@@ -81,6 +81,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "4d2a567c086d07d567943ed0f435616c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
